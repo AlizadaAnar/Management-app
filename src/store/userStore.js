2d67345d7c77d93455442dc3d4a4c231074a72ab
anaralizada <anar_alizade@internet.ru>
2023-12-17 22:22:58 +0400

    aded

---
+++ src/store/userStore.js
@@ -0,0 +1,23 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    users: []
+}
+
+let users;
+fetch('https://callous-knot-production.up.railway.app/api/v1/workers')
+    .then(response => response.json())
+    .then(commits => users = commits);
+
+
+export const userList = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+
+    }
+
+})
+
+
+export default userList.reducer
